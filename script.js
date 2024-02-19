30ef637e050e6dfb9e7f5f9eca082ef002616e5f
JT <143206481+JohnTHancock@users.noreply.github.com>
2024-02-19 12:39:25 -0500

    Set up logic to play game and place tokens
    The script has been set up to create an array and then place the player
    tokens within the array. The game works to an extent, but the win
    conditions are not yet set up.

---
+++ script.js
@@ -0,0 +1,93 @@
+function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        };
+    };
+
+    const getBoard = () => board;
+
+    const playToken = (selectedRow, selectedColumn, player) => {
+        const row = board[selectedRow];
+        const playedCell = row[selectedColumn];
+        if (playedCell.getValue() === 0) {
+            board[selectedRow][selectedColumn].addToken(player);
+        } else {
+            console.log('Invalid move');
+            return;
+        };
+    };
+
+    const printBoard = () => {
+        const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()));
+        console.log(boardWithCellValues);
+    };
+
+    return { getBoard, playToken, printBoard };
+};
+
+function Cell() {
+    let value = 0;
+
+    const addToken = (player) => {
+        value = player;
+    };
+
+    const getValue = () => value;
+
+    return {
+        addToken,
+        getValue
+    };
+}
+
+function GameController(
+    playerOneName = 'Player One',
+    playerTwoName = 'Player Two'
+) {
+    const board = Gameboard();
+
+    const players = [
+        {
+            playerName: playerOneName,
+            token: 1
+        },
+        {
+            playerName: playerTwoName,
+            token: 2
+        }
+    ];
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer().playerName}'s turn.`);
+    };
+
+    const playRound = (row, column) => {
+        board.playToken(row, column, getActivePlayer().token);
+
+        switchPlayerTurn();
+        printNewRound();
+    };
+
+    printNewRound();
+
+    return {
+        playRound,
+        getActivePlayer
+    };
+}
+
+const game = GameController();

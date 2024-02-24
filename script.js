452626c7badd3825b44e0450e2efbd0d544eb415
JT <143206481+JohnTHancock@users.noreply.github.com>
2024-02-24 17:29:14 -0500

    Add win conditions
    Added win conditions to check for rows, columns, and diagonals. Also
    added a condition that will announce a draw if the board is filled but
    no win condition is met.

--- script.js
+++ script.js
@@ -31,6 +31,16 @@ function Gameboard() {
     return { getBoard, playToken, printBoard };
 };
 
+
+// function findCellValue(selectedRow, selectedColumn) {
+//     const board = Gameboard();
+//     board.getBoard();
+//     // const row = board[selectedRow];
+//     // const selectedCell = row[selectedColumn];
+//     selectedCell = board[selectedRow][selectedColumn].Cell.getValue();
+//     console.log(selectedCell);
+// }
+
 function Cell() {
     let value = 0;
 
@@ -77,6 +87,109 @@ function GameController(
 
     const playRound = (row, column) => {
         board.playToken(row, column, getActivePlayer().token);
+
+        // const checkForRows = () => {
+        //     const boardArray = board.getBoard();
+        //     for (let i = 0; i < 3; i++) {
+        //         let row = [];
+        //         for (let j = 0; j < 3; j++) {
+        //             row.push(boardArray[i][j].getValue());
+        //         }
+
+        //         if (row.every(field => field == 1) || row.every(field => field == 2)) {
+        //             return true
+        //         }
+        //     }
+        //     return false
+        // }
+
+        // const checkforColumns = () => {
+        //     const boardArray = board.getBoard();
+        //     for (let i = 0; i < 3; i++) {
+        //         const firstCellValue = boardArray[0][i].getValue();
+
+        //         if (firstCellValue != 0 &&
+        //             boardArray[1][i].getValue() === firstCellValue &&
+        //             boardArray[2][i].getValue() === firstCellValue) {
+        //                 return true;
+        //             }
+        //     }
+        //     return false;
+        // }
+
+        // const checkForDiagonals = () => {
+        //     const boardArray = board.getBoard();
+        //     const topLeftValue = boardArray[0][0].getValue();
+        //     const topRightValue = boardArray[0][2].getValue();
+        //     if (topLeftValue != 0 &&
+        //         boardArray[1][1].getValue() === topLeftValue &&
+        //         boardArray[2][2].getValue() === topLeftValue) {
+        //             return true;
+        //         } else if (topRightValue != 0 &&
+        //             boardArray[1][1].getValue() === topRightValue &&
+        //             boardArray[2][0].getValue() === topRightValue) {
+        //                 return true;
+        //             }
+        //             return false;
+        // }
+
+        const checkForWin = () => {
+            const boardArray = board.getBoard();
+        
+            for (let i = 0; i < 3; i++) {
+                if (boardArray[i][0].getValue() !== 0 &&
+                    boardArray[i][0].getValue() === boardArray[i][1].getValue() &&
+                    boardArray[i][0].getValue() === boardArray[i][2].getValue()) {
+                    return true;
+                }
+            }
+        
+            for (let j = 0; j < 3; j++) {
+                if (boardArray[0][j].getValue() !== 0 &&
+                    boardArray[0][j].getValue() === boardArray[1][j].getValue() &&
+                    boardArray[0][j].getValue() === boardArray[2][j].getValue()) {
+                    return true;
+                }
+            }
+        
+            if (boardArray[0][0].getValue() !== 0 &&
+                boardArray[0][0].getValue() === boardArray[1][1].getValue() &&
+                boardArray[0][0].getValue() === boardArray[2][2].getValue()) {
+                return true;
+            }
+        
+            if (boardArray[0][2].getValue() !== 0 &&
+                boardArray[0][2].getValue() === boardArray[1][1].getValue() &&
+                boardArray[0][2].getValue() === boardArray[2][0].getValue()) {
+                return true;
+            }
+        
+            return false;
+        };
+
+        const checkForDraw = () => {
+            boardArray = board.getBoard();
+            for (let i = 0; i < 3; i++) {
+                for (let j = 0; j < 3; j++) {
+                    if (boardArray[i][j].getValue() === 0) {
+                        return false;
+                    }
+                }
+            }
+            return !checkForWin();
+        }
+        
+        const winnerFound = checkForWin();
+        if (winnerFound) {
+            console.log(`${getActivePlayer().playerName} wins!`);
+            return;
+        }
+
+        const drawFound = checkForDraw();
+        if (drawFound) {
+            console.log(`It's a draw!`);
+            return;
+        }
 
         switchPlayerTurn();
         printNewRound();
